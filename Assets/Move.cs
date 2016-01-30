a6e88f0e556f6b6417c7983c80a834aac635ec6e
Matthew Lindsay <thespian42@gmail.com>
2016-01-29 22:28:45 -0500

    Added basic movement and temporary art

---
+++ Assets/Move.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Move : MonoBehaviour {
+
+	float speed;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+		speed = 2.0f;
+		if(Input.GetKey(KeyCode.LeftShift))
+		{
+				speed *= 2.0f;
+		}
+
+		if (Input.GetKey (KeyCode.A)) {
+			this.gameObject.transform.position -= new Vector3(speed, 0, 0) * Time.deltaTime;
+		}
+
+		if (Input.GetKey (KeyCode.D)) {
+			this.gameObject.transform.position += new Vector3(speed, 0, 0) * Time.deltaTime;
+		}
+
+		if (Input.GetKey (KeyCode.W)) {
+			this.gameObject.transform.position += new Vector3(0, speed, 0) * Time.deltaTime;
+		}
+
+		if (Input.GetKey (KeyCode.S)) {
+			this.gameObject.transform.position -= new Vector3(0, speed, 0) * Time.deltaTime;
+		}
+
+	}
+}
