745d70c84eb60c4e5076ab9dfa52fa0c81c108b9
Brian Barnes <brian@decisivebison.com>
2016-01-30 13:46:42 -0500

    Textbox with "typing" text display
    Shows text on "start" function

---
+++ Assets/Scripts/DisplayText.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class DisplayText : MonoBehaviour {
+
+    public float letterPause = 0.03f;
+    string messageText;
+    string text;
+
+	// Use this for initialization
+    void Start()
+    {
+        this.SendMessage("updateText", "blah blah blah");
+    }
+
+    void updateText(string message)
+    {
+        messageText = message;
+        text = "";
+        StartCoroutine(TypeText());
+    }
+
+    IEnumerator TypeText()
+    {
+        foreach (char letter in messageText.ToCharArray())
+        {
+            text += letter;
+            GetComponent<Text>().text = text;
+            //if (sound)
+            //    audio.PlayOneShot(sound);
+            yield return 0;
+            yield return new WaitForSeconds(letterPause);
+        }
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
