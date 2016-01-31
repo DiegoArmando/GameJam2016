27736b2b863030e427e5378c16e78a1d4674d175
Brian Barnes <brian@decisivebison.com>
2016-01-30 21:24:34 -0500

    priest script support
    The priest now has a script file. It contains lines of text, as well as
    control codes for wait, text speed, and text color.

--- Assets/Scripts/DisplayText.cs
+++ Assets/Scripts/DisplayText.cs
@@ -1,24 +1,46 @@
 ﻿using UnityEngine;
 using System.Collections;
 using UnityEngine.UI;
+using System.Collections.Generic;
 
 public class DisplayText : MonoBehaviour {
 
     public float letterPause = 0.03f;
     string messageText;
     string text;
+    Color textColor;
+
 
 	// Use this for initialization
     void Start()
     {
-        this.SendMessage("updateText", "blah blah blah");
+        //this.SendMessage("updateText", "blah blah blah");
+    }
+
+    void setRate(int rate)
+    {
+        letterPause = (float)rate * 0.01f;
+    }
+
+    void setColor(string color)
+    {
+        List<float> components = new List<float>();
+        foreach (string c in color.Split(','))
+        {
+            float c_float;
+            float.TryParse(c, out c_float);
+            components.Add(c_float);
+        }
+        textColor = new Color(components[0], components[1], components[2], 1.0f);
+        GetComponent<Text>().color = textColor;
     }
 
     void updateText(string message)
     {
+        
         messageText = message;
         text = "";
-        StartCoroutine(TypeText());
+        StartCoroutine(TypeText()); 
     }
 
     IEnumerator TypeText()
@@ -29,7 +51,6 @@ public class DisplayText : MonoBehaviour {
             GetComponent<Text>().text = text;
             //if (sound)
             //    audio.PlayOneShot(sound);
-            yield return 0;
             yield return new WaitForSeconds(letterPause);
         }
     }
---
+++ Assets/Scripts/Priest.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Text;
+using System.IO;
+
+public class Priest : MonoBehaviour {
+    public GameObject TextBox;
+	// Use this for initialization
+	void Start () {
+        StartCoroutine(PriestScript());
+	}
+
+    IEnumerator PriestScript()
+    {
+        //TextBox.SendMessage("updateText", "Here's what I'm saying at the beginning.");
+        //yield return new WaitForSeconds(5);
+        //TextBox.SendMessage("updateText", "Line Two");
+        StreamReader reader = new StreamReader("assets\\priest.txt", Encoding.Default);
+        string line;
+        do
+        {
+            line = reader.ReadLine();
+            if (line != null)
+            {
+                if (line.Substring(0,1) == "<")
+                {
+                    switch (line.Substring(1, 4))
+                    {
+                        case "wait":
+                            int seconds;
+                            int.TryParse(line.Substring(6, line.Length - 7), out seconds);
+                            yield return new WaitForSeconds(seconds);
+                            break;
+
+                        case "rate":
+                            int rate;
+                            int.TryParse(line.Substring(6, line.Length - 7), out rate);
+                            TextBox.SendMessage("setRate", rate);
+                            break;
+
+                        case "colr":
+                            string colornum = line.Substring(6, line.Length - 7);
+                            TextBox.SendMessage("setColor", colornum);
+                            break;
+                    }
+                }
+                else
+                {
+                    TextBox.SendMessage("updateText", line);
+                }
+            }
+            yield return 0;
+        } while (line != null);
+    }
+    
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
