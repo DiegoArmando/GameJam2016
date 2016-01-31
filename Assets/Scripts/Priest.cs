d984b7a231f912121ab5e2563ad60fab2711c1f1
Brian Barnes <brian@decisivebison.com>
2016-01-31 02:33:32 -0500

    priest scripts
    Priest now has scripts for every occasion. This commit is at 2:30 AM.
    I'm so tired. Please let me rest.

--- Assets/Scripts/Priest.cs
+++ Assets/Scripts/Priest.cs
@@ -5,51 +5,82 @@ using System.IO;
 
 public class Priest : MonoBehaviour {
     public GameObject TextBox;
+    private bool interrupted = false;
+
 	// Use this for initialization
 	void Start () {
         StartCoroutine(PriestScript());
 	}
 
-    IEnumerator PriestScript()
+    WaitForSeconds ParseLine(string line)
     {
-        //TextBox.SendMessage("updateText", "Here's what I'm saying at the beginning.");
-        //yield return new WaitForSeconds(5);
-        //TextBox.SendMessage("updateText", "Line Two");
-        StreamReader reader = new StreamReader("assets\\priest.txt", Encoding.Default);
-        string line;
+        float waitTime = 0f;
+
+        if (line.Substring(0, 1) == "<")
+        {
+            switch (line.Substring(1, 4))
+            {
+                case "wait":
+                    int seconds;
+                    int.TryParse(line.Substring(6, line.Length - 7), out seconds);
+                    waitTime = seconds;
+                    break;
+
+                case "rate":
+                    int rate;
+                    int.TryParse(line.Substring(6, line.Length - 7), out rate);
+                    TextBox.SendMessage("setRate", rate);
+                    break;
+
+                case "colr":
+                    string colornum = line.Substring(6, line.Length - 7);
+                    TextBox.SendMessage("setColor", colornum);
+                    break;
+            }
+        }
+        else
+        {
+            TextBox.SendMessage("updateText", line);
+        }
+
+        return new WaitForSeconds(waitTime);
+    }
+
+    IEnumerator InterruptScript(string scriptFile)
+    {
+        StreamReader reader = new StreamReader("assets\\" + scriptFile, Encoding.Default);
+        string line = "";
+        interrupted = true;
         do
         {
-            line = reader.ReadLine();
-            if (line != null)
+            if (!interrupted)
             {
-                if (line.Substring(0,1) == "<")
+                line = reader.ReadLine();
+                if (line != null)
                 {
-                    switch (line.Substring(1, 4))
-                    {
-                        case "wait":
-                            int seconds;
-                            int.TryParse(line.Substring(6, line.Length - 7), out seconds);
-                            yield return new WaitForSeconds(seconds);
-                            break;
-
-                        case "rate":
-                            int rate;
-                            int.TryParse(line.Substring(6, line.Length - 7), out rate);
-                            TextBox.SendMessage("setRate", rate);
-                            break;
-
-                        case "colr":
-                            string colornum = line.Substring(6, line.Length - 7);
-                            TextBox.SendMessage("setColor", colornum);
-                            break;
-                    }
+                    yield return ParseLine(line);
                 }
-                else
-                {
-                    TextBox.SendMessage("updateText", line);
-                }
+                yield return 0;
             }
-            yield return 0;
+        } while (line != null);
+        interrupted = false;
+    }
+
+    IEnumerator PriestScript()
+    {
+        StreamReader reader = new StreamReader("assets\\wrongbop.txt", Encoding.Default);
+        string line = "";
+        do
+        {
+            if (!interrupted)
+            {
+                line = reader.ReadLine();
+                if (line != null)
+                {
+                    yield return ParseLine(line);
+                }
+                yield return 0;
+            }
         } while (line != null);
     }
     
