ac28281f21933e385bdb336a593fa88c205583c4
Matthew Lindsay <thespian42@gmail.com>
2016-01-31 13:16:18 -0500

    Finished bopping, fixed soft lock

---
+++ Assets/RefuseTrigger.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RefuseTrigger : MonoBehaviour {
+
+	public GameObject player;
+	public GameObject preRefuse;
+	public GameObject postRefuse;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnTriggerEnter2D(){
+		if (player.GetComponent<Move> ().fishFed) {
+			preRefuse.GetComponent<SpriteRenderer> ().sprite = postRefuse.GetComponent<SpriteRenderer> ().sprite;
+		}
+	}
+}
--- Assets/Scripts/Move.cs
+++ Assets/Scripts/Move.cs
@@ -32,21 +32,43 @@ public class Move : MonoBehaviour {
 	public GameObject bop5;
 	public GameObject bop6;
 
+	public GameObject boppee1;
+	public GameObject boppee2;
+	public GameObject boppee3;
+	public GameObject boppee4;
+	public GameObject boppee5;
+	public GameObject boppee6;
+
+	public GameObject postBop1;
+	public GameObject postBop2;
+	public GameObject postBop3;
+	public GameObject refuseBop;
+	public GameObject postBop5;
+	public GameObject postBop6;
+
+	public GameObject bopSprite;
+	Sprite originalFish;
+
+	float bopTimer = 0.34f;
+	bool bopping = false;
+
 	float timer = 4.0f;
 	bool waiting = false;
 
 	//public GameObject candle1;
 	//public 
 
-	bool candlesLit = false;
-	bool bookRead = false;
-	bool fishFed = false;
-	bool peopleBopped = false;
-	bool candlesUnlit = false;
+	public bool candlesLit = false;
+	public bool bookRead = false;
+	public bool fishFed = false;
+	public bool peopleBopped = false;
+	public bool candlesUnlit = false;
+
+	bool fadeChant = false;
 
 	// Use this for initialization
 	void Start () {
-	
+		originalFish = heldPish.GetComponent<SpriteRenderer> ().sprite;
 	}
 	
 	// Update is called once per frame
@@ -64,6 +86,10 @@ public class Move : MonoBehaviour {
 			bookRead = true;
 		}
 
+		if (fadeChant && heldPish.GetComponent<AudioSource> ().volume > 0) {
+			heldPish.GetComponent<AudioSource> ().volume -= Time.deltaTime / 3.0f;
+		}
+
 		if (transform.position.y > 2.9)
 		{
 			GetComponent<SpriteRenderer>().sortingLayerName = "Behind Stuff";
@@ -74,6 +100,27 @@ public class Move : MonoBehaviour {
 		}
 
 		speed = 5.0f;
+
+		if (heldPish.GetComponent<PoleBehavior> ().held) {
+			speed = 3.0f;
+		}
+
+
+		if (bopping) {
+			heldPish.GetComponent<SpriteRenderer> ().sprite = bopSprite.GetComponent<SpriteRenderer> ().sprite;
+			heldPish.transform.position = transform.position + new Vector3(0f, -2.5f);
+			speed = 0;
+			if (bopTimer > 0)
+				bopTimer -= Time.deltaTime;
+			else {
+				bopTimer = 0.34f;
+				bopping = false;
+
+				heldPish.GetComponent<SpriteRenderer> ().sprite = originalFish;
+				heldPish.transform.position = transform.position + new Vector3(1.0f, 0);
+			}
+		}
+
 		if (Input.GetKey (KeyCode.LeftShift)) {
 			speed *= 2.0f;
 			running = true;
@@ -185,9 +232,14 @@ public class Move : MonoBehaviour {
 
 
 			if (!fishFed) {
-				if (foodHome.GetComponent<Overlap> ().overlap && holding == false) {
-					food.SendMessage ("PickUp");
-					holding = true;
+				if (foodHome.GetComponent<Overlap> ().overlap) {
+					if (holding == false) {
+						food.SendMessage ("PickUp");
+						holding = true;
+					} else {
+						food.SendMessage ("PutDown");
+						holding = false;
+					}
 				}
 
 				if (foodSpot.GetComponent<Overlap> ().overlap && food.GetComponent<PoleBehavior> ().held && bookRead) {
@@ -198,11 +250,12 @@ public class Move : MonoBehaviour {
 					//Hide the pish
 					pish.GetComponent<SpriteRenderer> ().enabled = false;
 					heldPish.GetComponent<PoleBehavior> ().SendMessage ("PickUp");
-
+					heldPish.GetComponent<AudioSource> ().Play ();
 				}
 			} else if (foodSpot.GetComponent<Overlap> ().overlap && heldPish.GetComponent<PoleBehavior> ().held && peopleBopped) 
 			{
 				heldPish.SendMessage ("PutDown");
+				fadeChant = true;
 				pish.GetComponent<SpriteRenderer> ().enabled = true;
 				holding = true;
 
@@ -231,19 +284,25 @@ public class Move : MonoBehaviour {
 			if (bop1.GetComponent<Overlap> ().overlap && heldPish.GetComponent<PoleBehavior> ().held)
 			{
 				bop1.GetComponent<Overlap> ().bopped = true;
+				boppee1.GetComponent<SpriteRenderer> ().sprite = postBop1.GetComponent<SpriteRenderer> ().sprite;
 				peopleBopped = checkBop ();
+				bopping = true;
 			}
 
 			if (bop2.GetComponent<Overlap> ().overlap && heldPish.GetComponent<PoleBehavior> ().held)
 			{
 				bop2.GetComponent<Overlap> ().bopped = true;
+				boppee2.GetComponent<SpriteRenderer> ().sprite = postBop2.GetComponent<SpriteRenderer> ().sprite;
 				peopleBopped = checkBop ();
+				bopping = true;
 			}
 
 			if (bop3.GetComponent<Overlap> ().overlap && heldPish.GetComponent<PoleBehavior> ().held)
 			{
 				bop3.GetComponent<Overlap> ().bopped = true;
+				boppee3.GetComponent<SpriteRenderer> ().sprite = postBop3.GetComponent<SpriteRenderer> ().sprite;
 				peopleBopped = checkBop ();
+				bopping = true;
 			}
 
 			if (bop4.GetComponent<Overlap> ().overlap && heldPish.GetComponent<PoleBehavior> ().held)
@@ -255,13 +314,17 @@ public class Move : MonoBehaviour {
 			if (bop5.GetComponent<Overlap> ().overlap && heldPish.GetComponent<PoleBehavior> ().held)
 			{
 				bop5.GetComponent<Overlap> ().bopped = true;
+				boppee5.GetComponent<SpriteRenderer> ().sprite = postBop5.GetComponent<SpriteRenderer> ().sprite;
 				peopleBopped = checkBop ();
+				bopping = true;
 			}
 
 			if (bop6.GetComponent<Overlap> ().overlap && heldPish.GetComponent<PoleBehavior> ().held)
 			{
 				bop6.GetComponent<Overlap> ().bopped = true;
+				boppee6.GetComponent<SpriteRenderer> ().sprite = postBop6.GetComponent<SpriteRenderer> ().sprite;
 				peopleBopped = checkBop ();
+				bopping = true;
 			}
 
 		}
