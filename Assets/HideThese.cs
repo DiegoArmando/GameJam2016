da3add5808f116c90b4973d4695497c2ea8ab9e5
Matthew Lindsay <thespian42@gmail.com>
2016-01-31 14:41:50 -0500

    Added more hide/show functionality

--- Assets/HideThese.cs
+++ Assets/HideThese.cs
@@ -8,13 +8,52 @@ public class HideThese : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
+		hideAll ();
+	}
+
+	public void hideAll()
+	{
 		foreach (GameObject g in hideMe) {
 			g.GetComponent<SpriteRenderer> ().enabled = false;
 		}
-
 	}
 
-
+	public void showPointers(string input)
+	{
+		switch (input) {
+		case "staff":
+			hideMe[9].GetComponent<SpriteRenderer> ().enabled = true;
+			break;
+		case "snuff":
+		case "light":
+			hideMe[10].GetComponent<SpriteRenderer> ().enabled = true;
+			hideMe[11].GetComponent<SpriteRenderer> ().enabled = true;
+			hideMe[12].GetComponent<SpriteRenderer> ().enabled = true;
+			hideMe[13].GetComponent<SpriteRenderer> ().enabled = true;
+			break;
+		case "book":
+			hideMe[0].GetComponent<SpriteRenderer> ().enabled = true;
+			break;
+		case "read":
+			hideMe[1].GetComponent<SpriteRenderer> ().enabled = true;
+			break;
+		case "food":
+			hideMe[8].GetComponent<SpriteRenderer> ().enabled = true;
+			break;
+		case "pool":
+		case "feed":
+			hideMe[7].GetComponent<SpriteRenderer> ().enabled = true;
+			break;
+		case "bop":
+			hideMe [2].GetComponent<SpriteRenderer> ().enabled = true;
+			hideMe [3].GetComponent<SpriteRenderer> ().enabled = true;
+			hideMe [4].GetComponent<SpriteRenderer> ().enabled = true;
+			hideMe [5].GetComponent<SpriteRenderer> ().enabled = true;
+			hideMe [6].GetComponent<SpriteRenderer> ().enabled = true;
+			hideMe [14].GetComponent<SpriteRenderer> ().enabled = true;
+			break;
+		}
+	}
 	
 	// Update is called once per frame
 	void Update () {
--- Assets/Scripts/Priest.cs
+++ Assets/Scripts/Priest.cs
@@ -6,10 +6,12 @@ using System.IO;
 public class Priest : MonoBehaviour {
     public GameObject TextBox;
     private bool interrupted = false;
+    public GameObject Player;
 
 	// Use this for initialization
 	void Start () {
         StartCoroutine(PriestScript());
+		//print(gameObject.GetComponent<HideThese> ().hideMe [2].ToString ());
 	}
 
     WaitForSeconds ParseLine(string line)
@@ -46,24 +48,51 @@ public class Priest : MonoBehaviour {
         return new WaitForSeconds(waitTime);
     }
 
+    public System.Collections.Generic.Dictionary<string, int> interruptions = new System.Collections.Generic.Dictionary<string, int>();
+
+    void Interrupt(string phase)
+    {
+        if (phase == "staff2")
+        {
+            phase = "staff";
+            interruptions["staff"] = 0;
+        }
+        if (interruptions.ContainsKey(phase))
+        {
+            interruptions[phase]++;
+        }
+        else
+        {
+            interruptions[phase] = 1;
+        }
+        if (interruptions[phase] > 3) interruptions[phase] = 3;
+		print ("boop");
+		print ("What we are sending: " + phase + interruptions [phase] + ".txt");
+		StartCoroutine(InterruptScript(phase + interruptions[phase] + ".txt"));
+		print ("post boopism");
+    }
+
     IEnumerator InterruptScript(string scriptFile)
     {
+		print ("Does streamreader brok everythinangds?");
         StreamReader reader = new StreamReader("assets\\" + scriptFile, Encoding.Default);
+		print ("inside interrupt with script " + scriptFile);
         string line = "";
-        interrupted = true;
-        do
-        {
-            if (!interrupted)
-            {
-                line = reader.ReadLine();
-                if (line != null)
-                {
-                    yield return ParseLine(line);
-                }
-                yield return 0;
-            }
-        } while (line != null);
-        interrupted = false;
+		if (!interrupted)
+		{
+			interrupted = true;
+			do
+			{
+				line = reader.ReadLine ();
+				if (line != null)
+				{
+					yield return ParseLine (line);
+				}
+				yield return 0;
+			} while (line != null);
+			interrupted = false;
+			timer = 7f;
+		}
     }
 
     IEnumerator PriestScript()
@@ -79,13 +108,89 @@ public class Priest : MonoBehaviour {
                 {
                     yield return ParseLine(line);
                 }
-                yield return 0;
-            }
+			}
+			yield return 0;
         } while (line != null);
     }
-    
+
+    public float timer = 7f;
 	// Update is called once per frame
 	void Update () {
-	
+        timer -= Time.deltaTime;
+        if (timer <= 0f)
+        {
+            timer = 7f;
+            if (!Player.GetComponent<Move>().candlesLit)
+            {
+                if (!Player.GetComponent<Move>().pole.GetComponent<PoleBehavior>().held)
+                {
+                    Interrupt("staff");
+                }
+                else
+                {
+                    Interrupt("light");
+                }
+            }
+            else //candles are lit
+            {
+                if (!Player.GetComponent<Move>().bookRead)
+                {
+                    if (!Player.GetComponent<Move>().book.GetComponent<PoleBehavior>().held)
+                    {
+                        Interrupt("book");
+                    }
+                    else
+                    {
+                        Interrupt("read");
+                    }
+                }
+                else //book is read
+                {
+                    if (!Player.GetComponent<Move>().fishFed)
+                    {
+                        if (!Player.GetComponent<Move>().food.GetComponent<PoleBehavior>().held)
+                        {
+                            Interrupt("food");
+                        }
+                        else
+                        {
+                            Interrupt("feed");
+                        }
+                    }
+                    else //fish is fed
+                    {
+                        if (!Player.GetComponent<Move>().peopleBopped)
+                        {
+                            Interrupt("bop");
+                        }
+                        else //people are bopped
+                        {
+                            if (Player.GetComponent<Move>().heldPish.GetComponent<PoleBehavior>().held)
+                            {
+                                Interrupt("pool");
+                            }
+                            else //fish is returned
+                            {
+                                if (!Player.GetComponent<Move>().candlesUnlit)
+                                {
+                                    if (!Player.GetComponent<Move>().pole.GetComponent<PoleBehavior>().held)
+                                    {
+                                        Interrupt("staff2");
+                                    }
+                                    else
+                                    {
+                                        Interrupt("snuff");
+                                    }
+                                }
+                                else //candles are snuffed
+                                {
+                                    //win state
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
 	}
 }
